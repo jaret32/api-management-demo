0cde55f0cb394602317d965492be17133c731b04
Jaret Boyer <JBoyer@pfl.com>
2021-06-24 07:56:35 -0600

    added a single unit test

---
+++ test/Order.Api.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Order.Api.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [Category("UnitTest")]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
