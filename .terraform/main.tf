15029d4f28b17b8668bf50586f99f2bde8b73d3d
Jaret Boyer <JBoyer@pfl.com>
2021-06-24 22:52:34 -0600

    updated terraform file

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -7,15 +7,17 @@ terraform {
   }
   
   required_providers {
-    azurem = {
-      source  = "hashicorp/azurem"
+    azurerm = {
+      source  = "hashicorp/azurerm"
       version = "=2.46.0"
     }
   }
 }
 
-provider "azurem" {
+provider "azurerm" {
   features {}
+
+  subscription_id = "ecb4f63c-b0e0-4b21-9227-a58fd02cc035"
 }
 
 resource "random_id" "server" {
@@ -26,47 +28,82 @@ resource "random_id" "server" {
   byte_length = 8
 }
 
-resource "azurem_resource_group" "main" {
-  name     = "user-boyer"
+resource "azurerm_resource_group" "main" {
+  name     = "api-management-demo"
   location = "Central US"
 }
 
-resource "azurem_app_service_plan" "main" {
-  name                = "jaretApiManagmentDemo-asp"
-  location            = azurem_resource_group.main.location
-  resource_group_name = azurem_resource_group.main.name
+# resource "azurerm_app_service_plan" "main" {
+#   name                = "api-management-demo-asp"
+#   location            = azurerm_resource_group.main.location
+#   resource_group_name = azurerm_resource_group.main.name
 
-  sku {
-    tier = "Basic"
-    size = "B1"
-  }
-}
+#   sku {
+#     tier = "Basic"
+#     size = "B1"
+#   }
+# }
 
-resource "azurem_app_service" "main" {
-  name                = "jaretApiManagmentDemo-appservice"
+# resource "azurerm_app_service" "main" {
+#   name                = "api-managment-demo-appservice"
+#   location            = azurerm_resource_group.main.location
+#   resource_group_name = azurerm_resource_group.main.name
+#   app_service_plan_id = azurerm_app_service_plan.main.id
+
+#   site_config {
+#     dotnet_framework_version = "v5.0"
+#   }
+# }
+
+# resource "azurerm_app_service_slot" "main" {
+#   name                = random_id.server.hex
+#   app_service_name    = azurerm_app_service.main.name
+#   location            = azurerm_resource_group.main.location
+#   resource_group_name = azurerm_resource_group.main.name
+#   app_service_plan_id = azurerm_app_service_plan.main.id
+
+#   site_config {
+#     dotnet_framework_version = "v5.0"
+#   }
+# }
+
+# resource "azurerm_app_service_active_slot" "main" {
+#   resource_group_name   = azurerm_resource_group.main.name
+#   app_service_name      = azurerm_app_service.main.name
+#   app_service_slot_name = azurerm_app_service_slot.main.name
+# }
+
+resource "azurerm_api_management" "main" {
+  name                = "api-management-demo-apim"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  app_service_plan_id = azurerm_app_service_plan.main.id
-
-  site_config {
-    dotnet_framework_version = "v5.0"
-  }
+  publisher_name      = "PFL"
+  publisher_email     = "is@pfl.com"
+  sku_name            = "Consumption_0"
 }
 
-resource "azurem_app_service_slot" "main" {
-  name                = random_id.server.hex
-  app_service_name    = azurem_app_service.main.name
-  location            = azurem_resource_group.main.location
-  resource_group_name = azurem_resource_group.main.name
-  app_service_plan_id = azurem_app_service_plan.main.id
-
-  site_config {
-    dotnet_framework_version = "v5.0"
-  }
+resource "azurerm_api_management_api_version_set" "main" {
+  name                = "api-version-2021-06-23"
+  resource_group_name = azurerm_resource_group.main.name
+  api_management_name = azurerm_api_management.main.name
+  display_name        = "API Version 2021-06-23"
+  versioning_scheme   = "Query"
+  version_query_name  = "api-version"
 }
 
-resource "azurem_app_service_active_slot" "main" {
-  resource_group_name   = azurem_resource_group.main.name
-  app_service_name      = azurem_app_service.main.name
-  app_service_slot_name = azurem_app_service_slot.main.name
+resource "azurerm_api_management_api" "main" {
+  name                = "order-api"
+  resource_group_name = azurerm_resource_group.main.name
+  api_management_name = azurerm_api_management.main.name
+  revision            = "1"
+  display_name        = "Order API"
+  path                = ""
+  protocols           = ["https"]
+  version             = "2021-06-23"
+  version_set_id      = azurerm_api_management_api_version_set.main.id
+
+  import {
+    content_format = "openapi"
+    content_value  = file(abspath("../.openapi/apis/order-api.yaml"))
+  }
 }
